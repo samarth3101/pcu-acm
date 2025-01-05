78830986227ae9c5836aeb28bd02394c6aa83bff
[Samarth Patil] <143065657+samarth3101@users.noreply.github.com>
2025-01-05 22:32:54 +0530

    fixed bugs.

--- app2.js
+++ app2.js
@@ -34,3 +34,13 @@ window.onload = function () {
         video.play(); 
     }, 4100); 
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        hamburger.classList.toggle("open");
+    });
+});
